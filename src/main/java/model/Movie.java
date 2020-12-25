360c5a45fdd6ea30f7ce58863f2244fd88f8c8b4
ugursenoll <ugursenoll96@gmail.com>
2020-12-26 00:11:50 +0300

    findAMoviesWhenTitlesArePartiallyMatched test case created and pass

--- src/main/java/model/Movie.java
+++ src/main/java/model/Movie.java
@@ -1,6 +1,14 @@
 package model;
 
+
 public class Movie {
+    private String title;
+
     public Movie(String title) {
+        this.title = title;
+    }
+
+    public String title() {
+        return title;
     }
 }
--- src/test/java/MovieStoreTest.java
+++ src/test/java/MovieStoreTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 
 
 public class MovieStoreTest {
@@ -16,6 +17,25 @@ public class MovieStoreTest {
          List<Movie> results = movieStore.findByPartialTitle("abc");
 
         assertThat(results.size(),is(0));
+    }
+
+    @Test
+    public void findAMoviesWhenTitlesArePartiallyMatched() throws Exception{
+        MovieStore movieStore = new MovieStore();
+        Movie harryPotter = new Movie("Harry Potter");
+
+        movieStore.add(harryPotter);
+        movieStore.add(new Movie("Star Wars"));
+        movieStore.add(new Movie("Wanted"));
+        movieStore.add(new Movie("The Lord Of Rings"));
+
+        movieStore.findByPartialTitle("abc");
+
+        List<Movie> results = movieStore.findByPartialTitle("arry");
+
+        assertThat(results.size(),is(1));
+        assertThat(results, contains(harryPotter));
+
     }
 
 }
