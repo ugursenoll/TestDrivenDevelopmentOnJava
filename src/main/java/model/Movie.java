a2de396086454db987e773e8673fab60f5010f41
ugursenoll <ugursenoll96@gmail.com>
2020-12-26 18:31:39 +0300

    Refactoring and clean code

--- src/main/java/model/Movie.java
+++ src/main/java/model/Movie.java
@@ -4,10 +4,12 @@ package model;
 public class Movie {
     private String title;
     private String director;
+    private int releaseYear;
 
-    public Movie(String title, String director) {
+    public Movie(String title, String director, int releaseYear) {
         this.title = title;
         this.director = director;
+        this.releaseYear = releaseYear;
     }
 
     public String title() {
@@ -16,5 +18,9 @@ public class Movie {
 
     public String director() {
         return director;
+    }
+
+    public int releaseYear() {
+        return releaseYear;
     }
 }
